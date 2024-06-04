acff99748154313f1f3cb9e026cbadc9e129f5b6
Thong Cam <thongcam02@gmail.com>
2024-06-04 12:12:16 +0200

    Refactor URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify";
@@ -7,5 +7,6 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  site: "https://thong.cam",
 });
