1fbb2970f5e7e6a42110c5d1f0d0f8b25660ca1b
Thong Cam <thongcam02@gmail.com>
2024-06-04 12:22:49 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: "server",
   adapter: netlify(),
-  site: "https://thong.cam",
+  site: "https://thong.cam"
 });
