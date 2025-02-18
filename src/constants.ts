7920342a8610a3803369a46e987c0c0641622ebd
Thong Cam <thongcam02@gmail.com>
2025-02-18 23:56:02 +0200

    Use PUBLIC env variable for front end components

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const cmsURL  = process.env.NODE_ENV === "development" ? "http://192.168.0.196:3000" : process.env.CMS_URL_PROD;
+export const cmsURL  = process.env.NODE_ENV === "development" ? "http://192.168.0.196:3000" : process.env.PUBLIC_CMS_URL_PROD;
