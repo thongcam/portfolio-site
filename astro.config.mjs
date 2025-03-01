f2058cd72a7b7de0847aebed8622b47b2055f83d
Thong Cam <thongcam02@gmail.com>
2025-03-01 18:34:56 +0200

    Update to use Edge Function

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
+import netlify from '@astrojs/netlify/edge-functions';
 
 import sitemap from "@astrojs/sitemap";
 
