cb50f203a881fd471fc1172ae21024aacc8f8943
Thong Cam <thongcam02@gmail.com>
2025-04-08 12:03:44 +0300

    Add lower image densities for faster image load

--- src/utils/useAstroImage.ts
+++ src/utils/useAstroImage.ts
@@ -13,7 +13,7 @@ export default async function useAstroImage(obj : SerializedLexicalNode) {
         await Promise.all(Object.entries(currentNode).map(async ([key, value]) => {   
           if (key === 'mimeType' && typeof(value) === "string" && value.includes("image")) {
             const currentUploadNode = currentNode as UploadNodeWithURL
-            const astroImage = await getImage({src: cmsURL + currentUploadNode.url, inferSize: true, format:"webp"})
+            const astroImage = await getImage({src: cmsURL + currentUploadNode.url, inferSize: true, format:"webp", densities: [0.5, 1]})
             currentUploadNode.url = astroImage.src
           } else if (typeof value === 'object' && value !== null) {
             await traverse(value); // Recursively traverse nested objects
